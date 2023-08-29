bb29197eb313d4b153100678524fd6f8bc0d861d
Alexander Blagodarnyi <albly@MacBook-Pro-Alexander.local>
2023-08-29 22:39:27 +0700

    simple dataseet has been added

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -153,6 +153,65 @@ def generate_diff_data(radar_data, mask):
     return dataset
 
 
+def generate_simple_data(radar_data, mask):
+    MASK_SUM = mask.sum()
+    zeros_in_mask = np.logical_and(mask, np.logical_not(radar_data))                    # zeros-mask inside river 
+
+    zero_percentage = np.sum(zeros_in_mask.astype(int), (1,2)) * 100 / MASK_SUM         # zeros ratio inside river
+    filled_images_idxs = np.where(zero_percentage < 0.1)                                # idxs of fully filled images inside river
+
+    dataset = []                                                                        # var where we store final data
+
+    for fill_idx in filled_images_idxs[0]:                                              # go through all filled images
+        output_image = torch.tensor([])                                                 # difference masks with unique data 
+        input_radar = torch.tensor([])
+        used_idxs = []                                                                  # idxs of radar data that have unique data
+        filled_mask_bool = np.zeros((825, 200))                                              # mask-flag 
+        filled_mask_days = np.zeros((825, 200)) 
+
+        target_img = radar_data[fill_idx]                                               # target filled image that we want to predict
+        mask = np.squeeze(mask)     
+
+        for j in range(1,14):                                                           # 2 weeks memory
+            if fill_idx - j < 0 : break                                                 # stop if we outside the memory
+
+            cur_img = radar_data[fill_idx - j]                                          # past image 
+            cur_mask = np.logical_and(mask, cur_img)                                    # past image inside river
+            
+            if np.sum(cur_mask) == 0:                                                   # if past image is empty inside river
+                        continue                                                        # ommit this past image
+
+            delta_cur = target_img - cur_img                                            # find differece between target and past image
+            delta_mask = np.logical_and(abs(delta_cur) > 1e-6, cur_mask)                # find non-zero samples mask of the difference
+
+            #zeros_in_deltas = np.logical_and(mask, np.logical_not(delta_mask))         
+            other_in_delats = np.logical_and(mask, delta_mask)                          # non-zero samples mask of the difference inside the river
+            
+            new_data_mask = np.logical_and(np.logical_not(filled_mask_bool), other_in_delats)# select samples that has no information
+            
+            if new_data_mask.astype(int).sum() != 0:                                    # if there is new info
+                data_layer = torch.from_numpy(target_img * new_data_mask).unsqueeze(0)   # select difference with new info
+                cur_radar = torch.from_numpy(cur_img * new_data_mask).unsqueeze(0)
+                
+                filled_mask_days = filled_mask_days + new_data_mask * 1/j
+                input_radar = torch.cat((input_radar, cur_radar), dim = 0)
+                output_image = torch.cat((output_image, data_layer), dim = 0)           # store difference with new info
+                used_idxs.append(fill_idx-j)                                            # store idx of data
+                filled_mask_bool = np.logical_or(filled_mask_bool, other_in_delats)               # fill mask about having data
+
+        current_data = {}
+        current_data['target_idx'] = fill_idx
+        current_data['input_data'] = input_radar.sum(0).unsqueeze(0)
+        current_data['input_idxs'] = used_idxs
+        current_data['target_image'] = output_image.sum(0).unsqueeze(0)
+        current_data['mask_days'] = torch.from_numpy(filled_mask_days).unsqueeze(0)
+
+        dataset.append(current_data)
+
+    return dataset
+
+
+
 class Difference_Dataset(Dataset):
     def __init__(self, dataset_dict):
         self.dataset = dataset_dict
@@ -163,6 +222,24 @@ class Difference_Dataset(Dataset):
     def __getitem__(self, index):
         input_data = self.dataset[index]['input_data']
         target_difference = self.dataset[index]['difference']
+        days_mask = self.dataset[index]['mask_days']
+        target_idx = self.dataset[index]['target_idx']
+        #input_idxs = self.dataset[index]['input_idxs']
+
+        return input_data, target_difference, days_mask, target_idx
+
+
+
+class Simple_Dataset(Dataset):
+    def __init__(self, dataset_dict):
+        self.dataset = dataset_dict
+
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, index):
+        input_data = self.dataset[index]['input_data']
+        target_difference = self.dataset[index]['target_image']
         days_mask = self.dataset[index]['mask_days']
         target_idx = self.dataset[index]['target_idx']
         #input_idxs = self.dataset[index]['input_idxs']
